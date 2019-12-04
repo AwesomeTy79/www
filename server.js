02db1f5990e1ac381abd2555d89e09d573e04b5c
Tyler Beckman <tbeckman530@gmail.com>
2019-12-04 15:40:57 +0000

    add stuff commit

--- server.js
+++ server.js
@@ -1,17 +1,63 @@
 "use strict";
 
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var https = require('https').createServer({
+// Modules
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const https = require('https').createServer({
   key: fs.readFileSync('./cert/privkey.pem'),
   cert: fs.readFileSync('./cert/fullchain.pem')
 }, app);
-var io = require("socket.io")(https);
+const io = require("socket.io")(https);
 const process = require('process');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+var session = require("express-session"),
+    bodyParser = require("body-parser");
+var crypto = require("crypto");
+var sqlite3 = require("sqlite3");
+var db = new sqlite3.Database('./users.db');
+// Middleware
+app.use(express.static("public"));
+app.use(session({ secret: "iuoiudwiouwqoiudwqiudioodiwquoiduiouqwuoidoihjheojpwiehjidoqcshjlksaj" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {console.log(req.headers['x-forwarded-for'] || req.connection.remoteAddress); next();});
+function hashPassword(password, salt) {
+  console.log(password + " " + salt)
+  var hash = crypto.createHash('sha256');
+  hash.update(password);
+  hash.update(salt);
+  return hash.digest('hex');
+}
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  db.get('SELECT salt FROM users WHERE username = ?', username, function(err, row) {
+    if (!row) return done(null, false);
+    var hash = hashPassword(password, row.salt);
+    console.log(hash);
+    db.get('SELECT username, id FROM users WHERE username = ? AND password = ?', username, hash, function(err, row) {
+      if (!row) return done(null, false);
+      return done(null, row);
+    });
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  return done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  db.get('SELECT id, username FROM users WHERE id = ?', id, function(err, row) {
+    if (!row) return done(null, false);
+    return done(null, row);
+  });
+});
 
 process.title = "WebServer";
-app.use(express.static("public"));
+
+// Pages
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/pages/index.html", function (err) {
     if (err) {
@@ -21,6 +67,11 @@ app.get('/', function (req, res) {
     }
   });
 });
+app.get('/login', (req, res) => {
+  res.send('<form action="/login" method="post"> <div> <label>Username:</label> <input type="text" name="username"/> </div> <div> <label>Password:</label> <input type="password" name="password"/> </div> <div> <input type="submit" value="Log In"/> </div> </form>');
+});
+app.post('/login', passport.authenticate('local', { successRedirect: '/good-login',
+                                                    failureRedirect: '/bad-login' }));
 app.get('/room', function(req, res){
   res.sendFile(__dirname + '/pages/chat/chat.html');
 });
