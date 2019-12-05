1c0e25f9882533619ec749daf2b5c4b4182128b1
Tyler Beckman <tbeckman530@gmail.com>
2019-12-05 18:47:29 +0000

    I don't even know

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ passport.use(new LocalStrategy(function(username, password, done) {
   db.get('SELECT salt FROM users WHERE username = ?', username, function(err, row) {
     if (!row) return done(null, false);
     var hash = hashPassword(password, row.salt);
-    console.log(hash);
     db.get('SELECT username, id FROM users WHERE username = ? AND password = ?', username, hash, function(err, row) {
       if (!row) return done(null, false);
       return done(null, row);
